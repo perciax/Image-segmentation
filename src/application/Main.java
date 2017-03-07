eafd24064a5415bc8afbb1049a507ddd729eb197
perciax <mateusz.percinski@gmail.com>
2017-03-07 18:29:50 +0100

    cleans Main.java, adds comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//Load FXML resource
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("JFX.fxml"));
-			Scene scene = new Scene(root,400,400);
+			//Create scene
+			Scene scene = new Scene(root,500,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//Configure scene
+			primaryStage.setTitle("Image segmentation");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
